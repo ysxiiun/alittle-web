c564a48df11556dbf2827e30e7e52c0c07d80e91
SeaSon <664876390@qq.com>
2022-05-17 22:56:29 +0800

    init

---
+++ src/main/java/cn/org/alittle/web/AlittleWebApplication.java
@@ -0,0 +1,14 @@
+package cn.org.alittle.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlittleWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlittleWebApplication.class, args);
+		System.out.println("Hello, world");
+	}
+
+}
---
+++ src/test/java/cn/org/alittle/web/AlittleWebApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.org.alittle.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlittleWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
